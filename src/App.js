d39ae6963f34d51d17bead9013953ad8e25312b1
John-Yip-HK <yip4work@gmail.com>
2021-10-06 17:15:00 +0800

    Created a rough interface

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Table } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 
 function App() {
   return (
-    <div id="drum-machine">
-      <div id="display">
-        <Container fluid>
-          <Row>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
-          </Row>
-        </Container>
-      </div>
-    </div>
+    <Container fluid id="outer-container">
+      <Container id="drum-machine">
+        <Row>
+          <Col>Power Switch</Col>
+          <Col id="display">Display</Col>
+          <Col>Bank Switch</Col>
+        </Row>
+        <Row>
+          <Col>Volume Knob</Col>
+        </Row>
+        <Row id="drum-pads">
+          <Table bordered>
+            <tbody>
+              <tr>
+                <td class="drum-pad">
+                  Q
+                  <audio
+                    id="Q"
+                    src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+                  />
+                </td>
+                <td class="drum-pad">
+                  W
+                  <audio id="W" src="" />
+                </td>
+                <td class="drum-pad">
+                  E
+                  <audio id="E" src="" />
+                </td>
+              </tr>
+              <tr>
+                <td class="drum-pad">
+                  A
+                  <audio id="A" src="" />
+                </td>
+                <td class="drum-pad">
+                  S
+                  <audio id="S" src="" />
+                </td>
+                <td class="drum-pad">
+                  D
+                  <audio id="D" src="" />
+                </td>
+              </tr>
+              <tr>
+                <td class="drum-pad">
+                  Z
+                  <audio id="Z" src="" />
+                </td>
+                <td class="drum-pad">
+                  X
+                  <audio id="X" src="" />
+                </td>
+                <td class="drum-pad">
+                  C
+                  <audio id="C" src="" />
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Row>
+      </Container>
+    </Container>
   );
 }
 
